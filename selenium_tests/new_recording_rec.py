270712be02ae21179c912ef9ab06c280627e676a
D Z <dz@Ds-MacBook-Air.local>
2022-07-29 09:55:50 +0300

    fix

--- selenium_tests/new_recording_rec.py
+++ selenium_tests/new_recording_rec.py
@@ -5,9 +5,4 @@ from time import sleep
 class RecorderTests(BaseCase):
     def test_recording(self):
         self.open("https://www.google.com/")
-        self.click("button#L2AGLb div")
-        self.type('input[name="q"]', "test")
-        self.click("body div:nth-of-type(3) form div")
-        self.click("body div:nth-of-type(3) form div div:nth-of-type(3) center input")
-        self.open("https://www.google.com/search?q=test")
         sleep(3)
