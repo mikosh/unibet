9e8d3dcffb445c2c7e38aaccd50a7ef847042d32
D Z <dz@Ds-MacBook-Air.local>
2022-07-29 10:24:17 +0300

    fix v2

--- selenium_tests/new_recording_rec.py
+++ selenium_tests/new_recording_rec.py
@@ -5,4 +5,5 @@ from time import sleep
 class RecorderTests(BaseCase):
     def test_recording(self):
         self.open("https://www.google.com/")
+        self.save_screenshot_to_logs()
         sleep(3)
